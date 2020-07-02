ad8d25c3566bc376dca43e4640fdd1c81f930c1c
Pierre Henri <payhache@gmail.com>
2020-07-02 09:13:54 +0200

    création class PC

---
+++ src/app/models/pc.ts
@@ -0,0 +1,29 @@
+export class Pc {
+  id: number;
+  modele: string;
+  marque: string;
+  type: string;
+  category: string;
+  prixAchat: number;
+  prixVente: number;
+  dateEntreStock: Date;
+  constructor(
+    id: number = null,
+    modele: string = null,
+    marque: string = null,
+    type: string = null,
+    category: string = null,
+    prixAchat: number = null,
+    prixVente: number = null,
+    dateEntreStock: Date = null
+  ) {
+    this.id = id;
+    this.modele = modele;
+    this.marque = marque;
+    this.type = type;
+    this.category = category;
+    this.prixAchat = prixAchat;
+    this.prixVente = prixVente;
+    this.dateEntreStock = dateEntreStock;
+  }
+}
